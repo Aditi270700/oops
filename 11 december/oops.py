b83520ca887f787d870b9b64edbc99e3009f7b56
aditi saudagar <aditisaudagar17@gmail.com>
2024-12-11 15:17:28 +0530

    oops

---
+++ 11 december/oops.py
@@ -0,0 +1,14 @@
+# oops -- to add real world entity to your programming is called oops
+'''
+properties of an object
+variable
+1. instance variable
+2. static variable/ class variable
+3. local variable
+
+behavior/action of an object
+method
+1. Instance method
+2. class method
+3. static method
+'''
