bbe236da026041d57bda141af7238233cfc825ce
aditi saudagar <aditisaudagar17@gmail.com>
2024-12-16 14:41:34 +0530

    local variable

--- 11 december/question7.py
+++ 11 december/question7.py
@@ -1,9 +1,12 @@
 x=50
 class Student:
+    def add1():
+        print(x)
+        print(x)
+        z=20
+        print(z)
     def add():
         x=20
-        print(x)
-    def add1():
         print(x)
 obj=Student
 obj.add()
---
+++ 11 december/question8.py
@@ -0,0 +1,15 @@
+x=10
+class cybrom:
+    global x
+    x=50
+    def add():
+        print(x)
+        z=100
+        print(z)
+        print(x)
+    def add1():
+        print(x)
+obj=cybrom
+obj.add()
+obj.add1()
+print(x)
